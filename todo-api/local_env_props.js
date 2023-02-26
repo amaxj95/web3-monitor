e02c9b0f61895361391cb27870cc95d6727c9a18
amaxj95 <amaxj95@gmail.com>
2023-02-25 16:34:36 -0800

    setups

--- todo-api/local_env_props.js
+++ todo-api/local_env_props.js
@@ -2,9 +2,9 @@
 module.exports = {
     postgresHost: 'localhost',
     postgresPort: 5432,
-    postgresDatabase: 'todo',
-    postgresUser: 'todo',
-    postgresPassword: 'todo1234',
+    postgresDatabase: 'web3_mon',
+    postgresUser: 'admin',
+    postgresPassword: 'web3Password',
     redisHost: 'localhost',
     redisPort: 6379,
     elasticHost: 'localhost',
