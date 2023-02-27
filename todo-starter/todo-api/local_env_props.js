7e27845bef7dcb11779e7d366314e8aeaa62d1c4
Aus10 <amaxj95@gmail.com>
2023-02-27 14:25:16 +0000

    initial commit

---
+++ todo-starter/todo-api/local_env_props.js
@@ -0,0 +1,12 @@
+// Environment specific configuration injected into the container
+module.exports = {
+    postgresHost: 'https://amaxj95-ideal-trout-4gjr9x997px37596-5432.preview.app.github.dev/',
+    postgresPort: 5432,
+    postgresDatabase: 'todo',
+    postgresUser: 'todo',
+    postgresPassword: 'todo1234',
+    redisHost: 'https://amaxj95-ideal-trout-4gjr9x997px37596-6379.preview.app.github.dev/',
+    redisPort: 6379,
+    elasticHost: 'https://amaxj95-ideal-trout-4gjr9x997px37596-9200.preview.app.github.dev/',
+    elasticPort: 9200
+};
